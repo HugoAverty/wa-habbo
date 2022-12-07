a7e856051131c908e6577c47d118a475e6b51ccb
Valdo <vteixeiraromao@gmail.com>
2022-12-07 19:25:57 +0000

    Added exit to WA + optimizations

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra"
 
-console.log('Script started successfully');
+console.log('Script started successfully')
 
-let currentPopup: any = undefined;
+let currentPopup: any = undefined
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
+    console.log('Scripting API ready')
     console.log('Player tags: ',WA.player.tags)
 
-    var globalSound = WA.sound.loadSound("https://github.com/HugoAverty/wa-habbo/raw/master/src/ocean.wav");
-    var config = {
-        volume : 0.5,
+    /*const globalSound = WA.sound.loadSound("ocean.wav")
+    const config = {
+        volume : 0.3,
         loop : true,
         rate : 1,
         detune : 1,
@@ -21,129 +21,139 @@ WA.onInit().then(() => {
         seek : 0,
         mute : false
     }
-    globalSound.play(config);
+    globalSound.play(config)
+*/
 
-    WA.room.onEnterLayer('Settings/zonePopupBoat').subscribe(() => {
-        currentPopup = WA.ui.openPopup("popupBoat","Coming soon !",[]);
+    // Popups
+    WA.room.area.onEnter("intro").subscribe(() => {
+        currentPopup = WA.ui.openPopup("introPopup", "Welcome Gen 1 holder! This is your community treasure island where we'll host events, gatherings and many experiences with you!", [])
     })
-    WA.room.onLeaveLayer('Settings/zonePopupBoat').subscribe(closePopUp)
+    WA.room.area.onLeave("intro").subscribe(closePopup)
 
-    WA.room.onEnterLayer('Settings/zonePopupEntranceBoat').subscribe(() => {
-        currentPopup = WA.ui.openPopup("popupEntranceBoat","Coming soon !",[]);
+    WA.room.area.onEnter("cave").subscribe(() => {
+        currentPopup = WA.ui.openPopup("cavePopup", "It looks like this cavern is blocked from within. There might be a way to open it. (coming soon)", [])
     })
-    WA.room.onLeaveLayer('Settings/zonePopupEntranceBoat').subscribe(closePopUp)
+    WA.room.area.onLeave("cave").subscribe(closePopup)
 
-    WA.room.onEnterLayer('Settings/zonePopupIntro').subscribe(() => {
-        currentPopup = WA.ui.openPopup("popupIntro","Welcome Gen 1 holder! This is your community treasur island where we'll host events, gatherings and many experiences with you!",[]);
+    WA.room.area.onEnter("exitToWA").subscribe(() => {
+        currentPopup = WA.ui.openPopup("exitToWAPopup", "In this cauldron is simmering a secret recipe from the future", [
+            {
+                label: 'Taste it...',
+                className: 'primary',
+                callback: () => WA.nav.goToPage("https://play.staging.workadventu.re/@/tcm/workadventure/wa-village#from-gen1"),
+            }
+        ])
     })
-    WA.room.onLeaveLayer('Settings/zonePopupIntro').subscribe(closePopUp)
+    WA.room.area.onLeave("exitToWA").subscribe(closePopup)
 
-
-    WA.room.onEnterLayer('Settings/zonePopupCave').subscribe(() => {
-        currentPopup = WA.ui.openPopup("popupCave","It looks like this cavern is blocked from within. There might be a way to open it (coming soon)",[]);
+    WA.room.area.onEnter("boatEntrance").subscribe(() => {
+        currentPopup = WA.ui.openPopup("boatEntrancePopup", "Coming soon!", [])
     })
-    WA.room.onLeaveLayer('Settings/zonePopupCave').subscribe(closePopUp)
+    WA.room.area.onLeave("boatEntrance").subscribe(closePopup)
 
-    WA.room.onEnterLayer('Settings/zoneBuilding').subscribe(() => {
-        WA.room.showLayer("InAbove/inBuilding1");
-        WA.room.showLayer("InAbove/inBuilding2");
-        WA.room.showLayer("Settings/inCollide");
-
-        WA.room.hideLayer("Settings/outCollide");
-        WA.room.hideLayer("OutAbove/outBuilding1");
-        WA.room.hideLayer("OutAbove/outBuilding2");
+    WA.room.area.onEnter("shipCockpit").subscribe(() => {
+        currentPopup = WA.ui.openPopup("shipCockpitPopup", "Coming soon!", [])
     })
+    WA.room.area.onLeave("shipCockpit").subscribe(closePopup)
 
-    WA.room.onLeaveLayer('Settings/zoneBuilding').subscribe(() => {
-        WA.room.hideLayer("Settings/inCollide");
-        WA.room.hideLayer("InAbove/inBuilding1");
-        WA.room.hideLayer("InAbove/inBuilding2");
 
-        WA.room.showLayer("Settings/outCollide");
-        WA.room.showLayer("OutAbove/outBuilding1");
-        WA.room.showLayer("OutAbove/outBuilding2");
+    // Layers
+    WA.room.area.onEnter("building").subscribe(() => {
+        WA.room.showLayer("InAbove/inBuilding1")
+        WA.room.showLayer("InAbove/inBuilding2")
+        WA.room.showLayer("Settings/inCollide")
+
+        WA.room.hideLayer("Settings/outCollide")
+        WA.room.hideLayer("OutAbove/outBuilding1")
+        WA.room.hideLayer("OutAbove/outBuilding2")
     })
+    WA.room.area.onLeave("building").subscribe(() => {
+        WA.room.hideLayer("Settings/inCollide")
+        WA.room.hideLayer("InAbove/inBuilding1")
+        WA.room.hideLayer("InAbove/inBuilding2")
 
-    WA.room.onEnterLayer('Settings/zoneTante').subscribe(() => {  
-        WA.room.showLayer("Settings/inCollide");
-        WA.room.showLayer("InUnder/inTante1");
-        WA.room.showLayer("InUnder/inTante2");
-        WA.room.showLayer("InAbove/inTante3");
-
-        WA.room.hideLayer("Settings/outCollide");
-        WA.room.hideLayer("OutUnder/outTante1");
-        WA.room.hideLayer("OutAbove/outTante2");
-    })
-
-    WA.room.onLeaveLayer('Settings/zoneTante').subscribe(() => {
-        WA.room.hideLayer("Settings/inCollide");
-        WA.room.hideLayer("InUnder/inTante1");
-        WA.room.hideLayer("InUnder/inTante2");
-        WA.room.hideLayer("InAbove/inTante3");
-
-        WA.room.showLayer("Settings/outCollide");
-        WA.room.showLayer("OutUnder/outTante1");
-        WA.room.showLayer("OutAbove/outTante2");
+        WA.room.showLayer("Settings/outCollide")
+        WA.room.showLayer("OutAbove/outBuilding1")
+        WA.room.showLayer("OutAbove/outBuilding2")
     })
 
+    WA.room.area.onEnter("tent").subscribe(() => {
+        WA.room.showLayer("Settings/inCollide")
+        WA.room.showLayer("InUnder/inTent1")
+        WA.room.showLayer("InUnder/inTent2")
+        WA.room.showLayer("InAbove/inTent3")
 
-    WA.room.onEnterLayer('Settings/zoneBoat').subscribe(() => {  
-        WA.room.showLayer("Settings/inCollide");
-        WA.room.showLayer("InUnder/inBoatFloor1");
-        WA.room.showLayer("InUnder/inBoatFloor2");
-        WA.room.showLayer("InUnder/inBoatWall1");
-        WA.room.showLayer("InUnder/inBoatWall2");
-        WA.room.showLayer("InUnder/inBoatDetails1");
-        WA.room.showLayer("InUnder/inBoatDetails2");
-        WA.room.showLayer("InAbove/inBoatAboveWall");
-        WA.room.showLayer("InAbove/inBoatAbove1");
-        WA.room.showLayer("InAbove/inBoatAbove2");
+        WA.room.hideLayer("Settings/outCollide")
+        WA.room.hideLayer("OutUnder/outTent1")
+        WA.room.hideLayer("OutAbove/outTent2")
+    })
+    WA.room.area.onLeave("tent").subscribe(() => {
+        WA.room.hideLayer("Settings/inCollide")
+        WA.room.hideLayer("InUnder/inTent1")
+        WA.room.hideLayer("InUnder/inTent2")
+        WA.room.hideLayer("InAbove/inTent3")
 
-        WA.room.hideLayer("Settings/outCollide");
-        WA.room.hideLayer("OutAbove/outBoatAbove1");
-        WA.room.hideLayer("OutAbove/outBoatAbove2");
-        WA.room.hideLayer("OutAbove/outBoatAbove3");
-        WA.room.hideLayer("OutAbove/outBoatAbove4");
-        WA.room.hideLayer("OutAbove/outBoatAbove5");
-        WA.room.hideLayer("OutAbove/outBoatAbove6");
-        WA.room.hideLayer("Clouds");
-        WA.room.hideLayer("Lights");
+        WA.room.showLayer("Settings/outCollide")
+        WA.room.showLayer("OutUnder/outTent1")
+        WA.room.showLayer("OutAbove/outTent2")
     })
 
-    WA.room.onLeaveLayer('Settings/zoneBoat').subscribe(() => {
-        WA.room.hideLayer("Settings/inCollide");
-        WA.room.hideLayer("InUnder/inBoatFloor1");
-        WA.room.hideLayer("InUnder/inBoatFloor2");
-        WA.room.hideLayer("InUnder/inBoatWall1");
-        WA.room.hideLayer("InUnder/inBoatWall2");
-        WA.room.hideLayer("InUnder/inBoatDetails1");
-        WA.room.hideLayer("InUnder/inBoatDetails2");
-        WA.room.hideLayer("InAbove/inBoatAboveWall");
-        WA.room.hideLayer("InAbove/inBoatAbove1");
-        WA.room.hideLayer("InAbove/inBoatAbove2");
+    WA.room.area.onEnter("insideShip").subscribe(() => {
+        WA.room.showLayer("Settings/inCollide")
+        WA.room.showLayer("InUnder/inBoatFloor1")
+        WA.room.showLayer("InUnder/inBoatFloor2")
+        WA.room.showLayer("InUnder/inBoatWall1")
+        WA.room.showLayer("InUnder/inBoatWall2")
+        WA.room.showLayer("InUnder/inBoatDetails1")
+        WA.room.showLayer("InUnder/inBoatDetails2")
+        WA.room.showLayer("InAbove/inBoatAboveWall")
+        WA.room.showLayer("InAbove/inBoatAbove1")
+        WA.room.showLayer("InAbove/inBoatAbove2")
 
-        WA.room.showLayer("Settings/outCollide");
-        WA.room.showLayer("OutAbove/outBoatAbove1");
-        WA.room.showLayer("OutAbove/outBoatAbove2");
-        WA.room.showLayer("OutAbove/outBoatAbove3");
-        WA.room.showLayer("OutAbove/outBoatAbove4");
-        WA.room.showLayer("OutAbove/outBoatAbove5");
-        WA.room.showLayer("OutAbove/outBoatAbove6");
-        WA.room.showLayer("Clouds");
-        WA.room.showLayer("Lights");
+        WA.room.hideLayer("Settings/outCollide")
+        WA.room.hideLayer("OutAbove/outBoatAbove1")
+        WA.room.hideLayer("OutAbove/outBoatAbove2")
+        WA.room.hideLayer("OutAbove/outBoatAbove3")
+        WA.room.hideLayer("OutAbove/outBoatAbove4")
+        WA.room.hideLayer("OutAbove/outBoatAbove5")
+        WA.room.hideLayer("OutAbove/outBoatAbove6")
+        WA.room.hideLayer("Clouds")
+        WA.room.hideLayer("Lights")
+    })
+    WA.room.area.onLeave("insideShip").subscribe(() => {
+        WA.room.hideLayer("Settings/inCollide")
+        WA.room.hideLayer("InUnder/inBoatFloor1")
+        WA.room.hideLayer("InUnder/inBoatFloor2")
+        WA.room.hideLayer("InUnder/inBoatWall1")
+        WA.room.hideLayer("InUnder/inBoatWall2")
+        WA.room.hideLayer("InUnder/inBoatDetails1")
+        WA.room.hideLayer("InUnder/inBoatDetails2")
+        WA.room.hideLayer("InAbove/inBoatAboveWall")
+        WA.room.hideLayer("InAbove/inBoatAbove1")
+        WA.room.hideLayer("InAbove/inBoatAbove2")
+
+        WA.room.showLayer("Settings/outCollide")
+        WA.room.showLayer("OutAbove/outBoatAbove1")
+        WA.room.showLayer("OutAbove/outBoatAbove2")
+        WA.room.showLayer("OutAbove/outBoatAbove3")
+        WA.room.showLayer("OutAbove/outBoatAbove4")
+        WA.room.showLayer("OutAbove/outBoatAbove5")
+        WA.room.showLayer("OutAbove/outBoatAbove6")
+        WA.room.showLayer("Clouds")
+        WA.room.showLayer("Lights")
     })
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+        console.log('Scripting API Extra ready')
+    }).catch(e => console.error(e))
 
-}).catch(e => console.error(e));
+}).catch(e => console.error(e))
 
-function closePopUp(){
+function closePopup(){
     if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
+        currentPopup.close()
+        currentPopup = undefined
     }
 }
 
